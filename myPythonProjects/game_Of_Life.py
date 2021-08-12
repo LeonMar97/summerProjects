e04ac2000be298fec0d9968d3c93b1bf54f9bc52
Leon Markovich <leonmarkovich97@gmail.com>
2021-08-12 07:11:13 +0300

    random colors for testing

--- myPythonProjects/game_Of_Life.py
+++ myPythonProjects/game_Of_Life.py
@@ -9,15 +9,30 @@ w,
 height=1080,
 width=1920,
 )
+rectangles=[]
 color=["red","yellow","black"]
-r1=c.create_rectangle(2,2,66,66,outline='#fb0')
 for x in range(0,30):
     for y in range(0,15):
         k=c.create_rectangle(2+x*66,2+y*66,2+((x+1)*66),2+((y+1)*66),outline='#fb0')
         c.itemconfig(k,fill=color[random.randint(0,2)])
+        rectangles.append(k)
+
+def colorChange():
+    for a in rectangles:  
+        if(c.itemcget(a,'fill')=='red'):
+            c.itemconfig(a,fill='yellow')
+        elif(c.itemcget(a,'fill')=='yellow'):
+            c.itemconfig(a,fill='black')
+        elif(c.itemcget(a,'fill')=='black'):
+            c.itemconfig(a,fill='red')   
         c.pack()
-# c.itemconfig(r1,fill='red')
-# c.pack()
+
+def rectangleFlick():
+    c.after(500,rectangleFlick)  
+    colorChange()
+
+rectangleFlick()
+
 # def colorChange():
 #     c.after(500,colorChange)   
 #     if(c.itemcget(r1,'fill')=='red'):
