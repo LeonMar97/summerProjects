b0cb50ba9496d325c8789889fa3900f59852a07f
Leon Markovich <leonmarkovich97@gmail.com>
2021-08-12 06:03:15 +0300

    random colors for testing

--- myPythonProjects/game_Of_Life.py
+++ myPythonProjects/game_Of_Life.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 w=Tk()
 w.title('Game Of Life')
 w.geometry('1920x1080')
@@ -8,17 +9,24 @@ w,
 height=1080,
 width=1920,
 )
+color=["red","yellow","black"]
 r1=c.create_rectangle(2,2,66,66,outline='#fb0')
-c.itemconfig(r1,fill='red')
-c.pack()
-def colorChange():
-    c.after(500,colorChange)   
-    if(c.itemcget(r1,'fill')=='red'):
-        c.itemconfig(r1,fill='yellow')
-    elif(c.itemcget(r1,'fill')=='yellow'):
-        c.itemconfig(r1,fill='black')
-    elif(c.itemcget(r1,'fill')=='black'):
-        c.itemconfig(r1,fill='red')   
-colorChange()
+for x in range(0,30):
+    for y in range(0,15):
+        k=c.create_rectangle(2+x*66,2+y*66,2+((x+1)*66),2+((y+1)*66),outline='#fb0')
+        c.itemconfig(k,fill=color[random.randint(0,2)])
+        c.pack()
+# c.itemconfig(r1,fill='red')
+# c.pack()
+# def colorChange():
+#     c.after(500,colorChange)   
+#     if(c.itemcget(r1,'fill')=='red'):
+#         c.itemconfig(r1,fill='yellow')
+#     elif(c.itemcget(r1,'fill')=='yellow'):
+#         c.itemconfig(r1,fill='black')
+#     elif(c.itemcget(r1,'fill')=='black'):
+#         c.itemconfig(r1,fill='red')   
+# colorChange()
+
 w.mainloop()
 
